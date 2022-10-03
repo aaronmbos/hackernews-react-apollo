0ef31e073900be281a5ab1d25ace64b5ec749b51
Aaron Bos <aaronmbos@gmail.com>
2022-10-02 19:40:08 -0500

    Finish up Filtering section

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import LinkList from "./LinkList";
 import Login from "./Login";
 import { Route, Routes } from "react-router-dom";
+import Search from "./Search";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </div>
     </div>
